bde9e462dbefb07387a29a66960c6a53f503ac0f
ralphld <ralph.ld1209@gmail.com>
2026-04-25 14:41:23 +0800

    fix deployment: postCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'static',
   site: 'https://code-it-ralph.github.io',
   base: '/servus-cafe',
-  vite: {
-    plugins: [/** @type {any} */ (tailwindcss())],
-  },
 });
